31f8b00395489873233fdf0b1b2e76c479ac91c2
Giulio Amato <giulioamato19.92@gmail.com>
2026-07-04 16:42:21 +0200

    fix: app example

--- apps/example/App.tsx
+++ apps/example/App.tsx
@@ -1,34 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ScrollView, Text, View, SafeAreaView } from 'react-native';
 import { MaterialOrb } from 'react-native-nitro-shaders';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.item}>
-        <MaterialOrb material="metal" style={styles.surface} />
-        <Text style={styles.label}>metal</Text>
-      </View>
-      <View style={styles.item}>
-        <MaterialOrb material="water" style={styles.surface} />
-        <Text style={styles.label}>water</Text>
-      </View>
-      <View style={styles.item}>
-        <MaterialOrb material="iridescent" style={styles.surface} />
-        <Text style={styles.label}>iridescent</Text>
-      </View>
-      <View style={styles.item}>
-        <MaterialOrb material="aura" style={styles.surface} />
-        <Text style={styles.label}>aura</Text>
-      </View>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safearea}>
+      <ScrollView style={{ flex: 1 }}>
+        <View style={styles.container}>
+          <View style={styles.item}>
+            <MaterialOrb material="metal" style={styles.surface} />
+            <Text style={styles.label}>metal</Text>
+          </View>
+          <View style={styles.item}>
+            <MaterialOrb material="water" style={styles.surface} />
+            <Text style={styles.label}>water</Text>
+          </View>
+          <View style={styles.item}>
+            <MaterialOrb material="iridescent" style={styles.surface} />
+            <Text style={styles.label}>iridescent</Text>
+          </View>
+          <View style={styles.item}>
+            <MaterialOrb material="aura" style={styles.surface} />
+            <Text style={styles.label}>aura</Text>
+          </View>
+          <StatusBar style="auto" />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safearea: {
+    flex: 1,
+    backgroundColor: '#f7f8fb',
+    
+  },
   container: {
     flex: 1,
+    paddingVertical: 64,
     backgroundColor: '#f7f8fb',
     alignItems: 'center',
     justifyContent: 'center',
